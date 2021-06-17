f052b65847cdfee9f31a622d29900448706b4e7e
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2021-06-17 18:06:13 +0200

    Added Database Integration (mySQL)

---
+++ LinkCutter/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinkCutter.Models;
+
+namespace LinkCutter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            BaseModel model = new BaseModel("Link Cutter");
+            return View(model);
+        }
+    }
+}
