5560b654f43ec9e46316cf81204723963ad5cce1
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2021-06-17 22:30:42 +0200

    Base funcionality is now working

--- LinkCutter/Controllers/HomeController.cs
+++ LinkCutter/Controllers/HomeController.cs
@@ -9,7 +9,6 @@ namespace LinkCutter.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("/")]
         public IActionResult Index()
         {
             BaseModel model = new BaseModel("Link Cutter");
@@ -28,6 +27,8 @@ namespace LinkCutter.Controllers
                 link.TimesUsed++;
                 db.SaveChanges();
             }
+            if (!link.Url.Contains("//"))
+                link.Url = "http://" + link.Url;
             return Redirect(link.Url);
         }
 
@@ -43,6 +44,40 @@ namespace LinkCutter.Controllers
                 model.Link = link;
             }
             return View(model);
+        }
+
+        [HttpPost]
+        [Route("/cut")]
+        public IActionResult Cut(string url = "")
+        {
+            if (HttpContext.Request.Headers["X-Requested-With"] != "XMLHttpRequest")
+                return BadRequest();
+            string code;
+            using(var db = new Database())
+            {
+                Link link = new Link();
+                link.Url = url;
+                do
+                {
+                    code = string.Empty;
+                    Random random = new Random();
+                    for (int i = 0; i < 5; i++)
+                    {
+                        int r = random.Next(36); char c;
+                        if (r < 10)
+                            c = (char)(r + 48);
+                        else
+                            c = (char)(r + 97 - 10);
+                        code += c;
+                    }
+                } while (db.Links.Where(l => l.Code == code).FirstOrDefault() != null);
+                link.Code = code;
+                if (!link.Url.Contains("//"))
+                    link.Url = "http://" + link.Url;
+                db.Links.Add(link);
+                db.SaveChanges();
+            }
+            return Content(code);
         }
     }
 }
