5d67f50c0d0276c6ada8d91eeede610bfd81d6b9
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2024-06-19 16:29:56 +0200

    feat: move db file

--- LinkCutter/Database/Database.cs
+++ LinkCutter/Database/Database.cs
@@ -10,10 +10,18 @@ namespace LinkCutter
     {
 
         public DbSet<Link> Links { get; set; }
+        public string DbPath { get; set; }
+
+        public Database()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "links.db");
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=links.db");
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
         }
     }
 }
